5e8f519b10a11b44c4d79179969010b2996c6e1d
mickoissicko <defualtusermick@gmail.com>
2024-08-30 22:47:10 +0200

    finish minimal transpiler (only headings rn)

--- src/parser/transpile.c
+++ src/parser/transpile.c
@@ -2,5 +2,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void Transpile(char String[])
-{}
+#define MAX_FILENAME_LENGTH 128
+#define MAX_LINE_LENGTH 8192
+
+void AppendString(char String[], const char FILE_PATH[])
+{
+    FILE* Result = fopen(FILE_PATH, "a");
+
+    if (Result == NULL)
+    {
+        perror("Could not open file");
+        fclose(Result);
+        exit(1);
+    }
+
+    String[strcspn(String, "\n")] = '\0';
+
+    fprintf(Result, "%s\n", String);
+    fclose(Result);
+}
+
+void Transpile(char String[], const char FILE_PATH[])
+{
+    char ProcessedFilePath[MAX_FILENAME_LENGTH];
+    snprintf(ProcessedFilePath, MAX_FILENAME_LENGTH, "%s.html", FILE_PATH);
+
+    AppendString(String, ProcessedFilePath);
+}
