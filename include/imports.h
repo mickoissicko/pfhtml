a7f3d8a08888adeec079c872c4056e3b748a04e0
mickoissicko <defualtusermick@gmail.com>
2024-08-30 22:46:59 +0200

    add new fn defs

--- include/imports.h
+++ include/imports.h
@@ -3,11 +3,11 @@
 
 #include <stdio.h>
 
-char* FileHandler(FILE* File, char String[], const char Mode[]);
+char* FileHandler(const char FILE_PATH[], FILE* File, char String[], const char Mode[]);
 char* AddTags(char Line[], const char Tag[], int Mode);
 
 void FileManager(char FilePath[], char String[], const char Mode[]);
-void Transpile(char String[]);
-void Parse(char Line[]);
+void Transpile(char String[], const char FILE_PATH[]);
+void Parse(char Line[], const char FILE_PATH[]);
 
 #endif //IMPORTS_H
