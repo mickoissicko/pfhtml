3a1c9dd695d74b5d630b2ddd999c700b9b5b3e12
mickoissicko <defualtusermick@gmail.com>
2024-09-01 21:20:22 +0200

    finish base parser

--- src/parser/md/md.c
+++ src/parser/md/md.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const static int True = 1;
+#define MAX_LINE_LENGTH 1024
 
 void MoveString(char String[], size_t Length)
 {
@@ -45,10 +45,56 @@ int WriteHTML(
     return EXIT_FAILURE;
 }
 
+void InsertBreaks(char Escape[], int Symbol, char Tag[], char Line[], const char FILE_PATH[])
+{
+    // if Line has $ but not \$, proceed
+
+    char Result[MAX_LINE_LENGTH];
+
+    char* Cursor = Line;
+    char* Buffer = Result;
+
+    while (*Cursor)
+    {
+        if (*Cursor == Symbol && (Cursor == Line || *(Cursor - 1) != '\\'))
+        {
+            strcpy(Buffer, Tag);
+            Buffer += strlen(Tag);
+        }
+
+        else
+            *Buffer++ = *Cursor;
+
+        Cursor++;
+    }
+
+    *Buffer = '\0';
+    strcpy(Line, Result);
+}
+
+void DeleteBackslashes(char Line[])
+{
+    int Index = 0, Cursor = 0;
+
+    while (Line[Index] != '\0')
+    {
+        if (Line[Index] != '\\')
+            Line[Cursor++] = Line[Index];
+
+        Index++;
+    }
+
+    Line[Cursor] = '\0';
+}
+
 void ParseMarkdown(char Line[], const char FILE_PATH[])
 {
-    // yea.. idk either man
+    InsertBreaks("\\$", '$', "<br>", Line, FILE_PATH);
 
+    if (strchr(Line, '\\') != NULL)
+        DeleteBackslashes(Line);
+
+    // yea.. idk either man
     if (WriteHTML(Line, FILE_PATH, "<h1>", "</h1>", "#") != 0)
         if (WriteHTML(Line, FILE_PATH, "<h2>", "</h2>", "##") != 0)
             if (WriteHTML(Line, FILE_PATH, "<ul><li>", "</li></ul>", ">") != 0)
